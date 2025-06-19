8a77ea3f43ff30d8a728e5ef1139e4302661c1dd
Srijana Kathayat <srijana.kathayat21@gmail.com>
2025-06-19 15:56:51 +0545

    first commit - added js, css, html file

---
+++ script.js
@@ -0,0 +1,16 @@
+
+AOS.init();
+
+
+// Back to Top button functionality
+const backToTopBtn = document.getElementById('backToTopBtn');
+window.onscroll = function () {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopBtn.style.display = "block";
+    } else {
+        backToTopBtn.style.display = "none";
+    }
+};
+backToTopBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
